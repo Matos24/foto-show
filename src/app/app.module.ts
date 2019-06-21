18bfef83695f756bd1d1a32aab1ccbfb2890930c
Matos24 <tomasmatos.frois@gmail.com>
2019-06-21 16:58:01 +0100

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TopComponent } from './top/top.component';
+import { ShowImgComponent } from './show-img/show-img.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TopComponent, ShowImgComponent, GridComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
